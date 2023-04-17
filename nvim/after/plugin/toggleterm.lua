fcf5f7b8e2433435a3d8bbf04158083ff8bb42ed
iainmcl <iain.mclaughlan@capita.com>
2023-04-17 10:09:26 +0100

    Set default shell to PowerSehll

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -16,7 +16,7 @@ toggleterm.setup({
     persist_size = true,
     direction = 'float',
     close_on_exit = true,
-    shell = vim.o.shell,
+    shell = 'pwsh',
     float_opts = {
         border = 'curved',
         winblend = 0
