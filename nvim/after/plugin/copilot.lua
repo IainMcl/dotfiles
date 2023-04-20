47f3ddc4ecac27a9c0c9ee303e6c2bf613df5be5
iainmcl <iain.mclaughlan@capita.com>
2023-04-20 14:47:05 +0100

    Update nvimtree options

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_set_keymap('n', '<leader>cp', ":Copilot<CR>", {noremap = true, desc="Copilot"})
+
--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -1,2 +1,14 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true
+    },
+    auto_reload_on_write = true,
+    sort_by = 'name',
+    view = {
+        width = 35,
+    }
+})
 vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeToggle<CR>', {noremap = true, desc="[T]oggle nvim tree"})
+
+
