dab49cad66934d07e4c5b4535dbe1c20e169e60c
IainMcl <34077975+Darthmazgar@users.noreply.github.com>
2024-06-02 18:54:13 +0100

    Trying to get trouble to show in lualine

--- nvim/lua/iainm/lazy/lualine.lua
+++ nvim/lua/iainm/lazy/lualine.lua
@@ -2,7 +2,25 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependancies = {
 		"nvim-tree/nvim-web-devicons",
+		-- "folke/trouble.nvim",
 	},
+	-- opts = function(_, opts)
+	-- 	local trouble = require("trouble")
+	-- 	local symbols = trouble.statusline({
+	-- 		mode = "lsp_document_symbols",
+	-- 		groups = {},
+	-- 		title = false,
+	-- 		filter = { range = true },
+	-- 		format = "{kind_icon}{symbol.name:Normal}",
+	-- 		-- The following line is needed to fix the background color
+	-- 		-- Set it to the lualine section you want to use
+	-- 		hl_group = "lualine_c_normal",
+	-- 	})
+	-- 	table.insert(opts.sections.lualine_c, {
+	-- 		symbols.get,
+	-- 		cond = symbols.has,
+	-- 	})
+	-- end,
 	config = function()
 		require("lualine").setup({
 			options = {
