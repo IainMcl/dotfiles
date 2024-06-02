ea3dcf2a2d38f4d8cb5bc44a910363eb14e07304
IainMcl <34077975+Darthmazgar@users.noreply.github.com>
2024-06-02 18:58:32 +0100

    Remove old files before main merge

--- nvim/tmp/old/after/plugin/colors.lua
+++
@@ -1,5 +0,0 @@
-
--- vim.o.background = "dark"
--- vim.cmd([[colorscheme gruvbox]])
-
-require("colorbuddy").colorscheme("cobalt2")
--- nvim/tmp/old/lua/iainm/init.lua
+++
@@ -1,5 +0,0 @@
-require("iainm.set")
-require("iainm.remap")
-require("iainm.packer")
-
-    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
