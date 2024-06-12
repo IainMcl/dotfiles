bc3547a1673d33c4acadcf6855bd9ee8d3eb0b69
IainMcl <iain.mclaughlan@yahoo.com>
2024-06-12 10:04:14 +0100

    Tidy up unused plugins and add theme picker

--- nvim/lua/iainm/lazy/colors.lua
+++ nvim/lua/iainm/lazy/colors.lua
@@ -1,52 +1,66 @@
-return{
-  'ribru17/bamboo.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('bamboo').setup {
-      -- optional configuration here
-    }
-    require('bamboo').load()
-  end,
-}
--- return {
--- 	"ellisonleao/gruvbox.nvim",
---
--- 	config = function()
--- 		-- Default options:
--- 		require("gruvbox").setup({
--- 			terminal_colors = true, -- add neovim terminal colors
--- 			undercurl = true,
--- 			underline = true,
--- 			bold = true,
--- 			italic = {
--- 				strings = true,
--- 				emphasis = true,
--- 				comments = true,
--- 				operators = false,
--- 				folds = true,
--- 			},
--- 			strikethrough = true,
--- 			invert_selection = false,
--- 			invert_signs = false,
--- 			invert_tabline = false,
--- 			invert_intend_guides = false,
--- 			inverse = true, -- invert background for search, diffs, statuslines and errors
--- 			contrast = "", -- can be "hard", "soft" or empty string
--- 			palette_overrides = {},
--- 			overrides = {},
--- 			dim_inactive = false,
--- 			transparent_mode = false,
--- 		})
--- 		vim.cmd("colorscheme gruvbox")
--- 	end,
--- }
+return {
+    {
+        'zaldih/themery.nvim',
+        config = function()
+            -- Minimal config
+            require("themery").setup({
+                themes = { "gruvbox", "bamboo", "tokyonight", "catppuccin", "kanagawa" }, -- Your list of installed colorschemes
+                themeConfigFile = "~/.config/nvim/lua/iainm/theme.lua",                   -- Described below
+                livePreview = true,                                                       -- Apply theme while browsing. Default to true.
+            })
+        end
+    },
+    {
+        'ribru17/bamboo.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('bamboo').setup {
+                -- optional configuration here
+            }
+            require('bamboo').load()
+        end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
 
--- return {
--- 	"folke/tokyonight.nvim",
--- 	priority = 1000,
--- 	init = function()
--- 		vim.cmd.colorscheme("tokyonight-night")
--- 		vim.cmd.hi("Comment gui=none")
--- 	end,
--- }
+        config = function()
+            -- Default options:
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "",  -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        init = function()
+            vim.cmd.colorscheme("tokyonight-night")
+            vim.cmd.hi("Comment gui=none")
+        end,
+    },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    "rebelot/kanagawa.nvim",
+
+}
