e4e683bd3d4edd2e1273b8d5c8689c46a9d4ecd8
IainMcl <iain.mclaughlan@travelperk.com>
2025-01-31 10:16:30 +0000

    Work changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -29,10 +29,10 @@ config.scrollback_lines = 3500
 -- config.window_decorations = "RESIZE"
 config.window_decorations = "TITLE | RESIZE"
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 -- wezterm.on("gui-startup", function(cmd)
 -- 	local _, _, window = wezterm.mux.spawn_window(cmd or {})
@@ -51,26 +51,26 @@ config.adjust_window_size_when_changing_font_size = false
 config.line_height = 1.1
 
 local function get_background_image()
-    -- Select a random background image from the BackgroundImages folder
-    local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
-    local background_dir = home_dir .. "/dotfiles/wezterm/BackgroundImages/"
-    local background_images = {
-        background_dir .. "firewatch_night.jpg",
-        -- background_dir .. "windows-error.jpg",
-        background_dir .. "salty_mountains.png",
-        background_dir .. "forrest.png",
-        background_dir .. "comfy-home.png",
-        background_dir .. "shaded_landscape.png",
-    }
-    -- for _, file in ipairs(wezterm.glob("*", { dir = background_dir })) do
-    -- 	if file:match("%.png$") or file:match("%.jpg$") then
-    -- 		table.insert(background_images, file)
-    -- 	end
-    -- end
-    if #background_images == 0 then
-        return nil
-    end
-    return background_images[math.random(#background_images)]
+	-- Select a random background image from the BackgroundImages folder
+	local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
+	local background_dir = home_dir .. "/dotfiles/wezterm/BackgroundImages/"
+	local background_images = {
+		background_dir .. "firewatch_night.jpg",
+		-- background_dir .. "windows-error.jpg",
+		background_dir .. "salty_mountains.png",
+		background_dir .. "forrest.png",
+		background_dir .. "comfy-home.png",
+		background_dir .. "shaded_landscape.png",
+	}
+	-- for _, file in ipairs(wezterm.glob("*", { dir = background_dir })) do
+	-- 	if file:match("%.png$") or file:match("%.jpg$") then
+	-- 		table.insert(background_images, file)
+	-- 	end
+	-- end
+	if #background_images == 0 then
+		return nil
+	end
+	return background_images[math.random(#background_images)]
 end
 
 local dimmer = { brightness = 0.1 }
@@ -87,9 +87,9 @@ local dimmer = { brightness = 0.1 }
 -- }
 -- config.window_background_image_mode = "Cover"
 config.window_background_image_hsb = {
-    brightness = 0.1,
-    hue = 1.0,
-    saturation = 1.0,
+	brightness = 0.1,
+	hue = 1.0,
+	saturation = 1.0,
 }
 -- config.macos_window_background_blur = 10
 -- config.window_background_opacity = 0.75
@@ -108,67 +108,132 @@ config.hide_tab_bar_if_only_one_tab = true
 -------------------
 
 config.mouse_bindings = {
-    -- Ctrl-click will open the link under the mouse cursor
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "CTRL",
-        action = wezterm.action.OpenLinkAtMouseCursor,
-    },
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
 }
 
 -------------------
 --- Keymaps
 -------------------
--- timeout_milliseconds defaults to 1000 and can be omitted
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
-    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-    {
-        key = "a",
-        mods = "LEADER|CTRL",
-        action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
-    },
-    {
-        key = "|",
-        mods = "LEADER|SHIFT",
-        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    {
-        key = "-",
-        mods = "LEADER|SHIFT",
-        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    { key = "-", mods = "CTRL",   action = wezterm.action.DecreaseFontSize },
-    { key = "+", mods = "CTRL",   action = wezterm.action.IncreaseFontSize },
-    { key = "0", mods = "CTRL",   action = wezterm.action.ResetFontSize },
-    { key = "t", mods = "LEADER", action = wezterm.action.ToggleFullScreen },
-    { key = "f", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
-    { key = "t", mods = "CTRL",   action = wezterm.action.SpawnTab("DefaultDomain") },
-    { key = "w", mods = "CTRL",   action = wezterm.action.CloseCurrentTab({ confirm = true }) },
-    {
-        key = "b",
-        mods = "LEADER",
-        action = wezterm.action_callback(function()
-            print("toggle background")
-            local new_background = get_background_image()
-            print(new_background)
-            if new_background then
-                print("new background")
-                wezterm.set_background_image(new_background)
-                -- wezterm.set_backbackground = {
-                -- 	{
-                -- 		source = {
-                -- 			File = new_background,
-                -- 		},
-                -- 		hs = dimmer,
-                -- 		vertical_align = "Bottom",
-                -- 		horizontal_align = "Center",
-                -- 	},
-                -- }
-            end
-        end),
-    },
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
 }
+
+local function split_nav(key)
+	return {
+		key = key,
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection(direction_keys[key]),
+		-- action = wezterm.action_callback(function(win, pane)
+		-- 	if pane:get_users_vars().IS_NVIM == "true" then
+		-- 		-- pass the keys through to vim/nvim
+		-- 		win:perform_action({
+		-- 			SendKey = { key = key, mods = "CTRL" },
+		-- 		}, pane)
+		-- 	else
+		-- 		win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+		-- 	end
+		-- end),
+	}
+end
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+	{ key = "+", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+	{ key = "t", mods = "LEADER", action = wezterm.action.ToggleFullScreen },
+	{ key = "f", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+	{ key = "t", mods = "CTRL", action = wezterm.action.SpawnTab("DefaultDomain") },
+	{ key = "w", mods = "CTRL", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+	{
+		key = '"',
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+
+	{
+		key = "%",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	-- { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+	{
+		key = "c",
+		mods = "LEADER",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+
+	{
+		key = "p",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.ShowTabNavigator,
+	},
+	split_nav("h"),
+	split_nav("j"),
+	split_nav("k"),
+	split_nav("l"),
+
+	-- {
+	-- 	key = "b",
+	-- 	mods = "LEADER",
+	-- 	action = wezterm.action_callback(function()
+	-- 		print("toggle background")
+	-- 		local new_background = get_background_image()
+	-- 		print(new_background)
+	-- 		if new_background then
+	-- 			print("new background")
+	-- 			wezterm.set_background_image(new_background)
+	-- 			-- wezterm.set_backbackground = {
+	-- 			-- 	{
+	-- 			-- 		source = {
+	-- 			-- 			File = new_background,
+	-- 			-- 		},
+	-- 			-- 		hs = dimmer,
+	-- 			-- 		vertical_align = "Bottom",
+	-- 			-- 		horizontal_align = "Center",
+	-- 			-- 	},
+	-- 			-- }
+	-- 		end
+	-- 	end),
+	-- },
+}
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
 
 return config
 
